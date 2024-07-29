37cc5a29c3e70bc220a9291b609a4c477a7be771
thangndtitf <thangndtitf@gmail.com>
2024-07-29 10:15:01 +0700

    main : COMMIT : Dung source buoc dau voi cac repository can thiet

---
+++ src/main/java/TradingPlace/tradingPlaceCore/TradingPlaceCoreApplication.java
@@ -0,0 +1,13 @@
+package TradingPlace.tradingPlaceCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingPlaceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingPlaceCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TradingPlace/tradingPlaceCore/TradingPlaceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package TradingPlace.tradingPlaceCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingPlaceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
